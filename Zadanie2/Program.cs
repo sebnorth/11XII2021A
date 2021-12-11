13c5f4a68bf430fbae5e37bf8f8c1b12d535b4f4
sebnorth <sebnorth@gmail.com>
2021-12-11 16:22:45 +0100

    Dodano temat pliki

---
+++ Zadanie2/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Zadanie2
+{
+    struct Osoba
+    {
+
+        public string imie;
+        public string nazwisko;
+        public string email;
+        public int telefon;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, s = 0;
+            Osoba os;
+            Console.WriteLine("Podaj imie: ");
+            os.imie = Console.ReadLine();
+            Console.WriteLine("Podaj nazwisko: ");
+            os.nazwisko = Console.ReadLine();
+            a = os.imie.Length + os.nazwisko.Length + 1;
+
+            Console.WriteLine("Podaj email: ");
+            os.email = Console.ReadLine();
+            os.email = "e-mail: " + os.email;
+            b = os.email.Length;
+
+            Console.WriteLine("Podaj numer telefonu(9cyfra liczba): ");
+            os.telefon = Convert.ToInt32(Console.ReadLine());
+            string napisTelefon = "tel. " + os.telefon;
+            c = napisTelefon.Length;
+            int dlugoscNajdluzszegoNapisu = Math.Max(Math.Max(a, b), c);
+            int dlugoscWizytowki = dlugoscNajdluzszegoNapisu + 4;
+
+
+            // fragment operacji na plikach
+            string nazwa = @"C:\Users\sagata\Downloads\inne\kursy\C#\zaj5\pliki10\zastepstwo\wizytowka.txt";
+
+            FileStream plik = new FileStream(nazwa, FileMode.Create, FileAccess.Write, FileShare.None);
+            System.IO.StreamWriter zapis = new StreamWriter(plik);
+
+
+            // pierwszy wiersz wizytówki
+            for (int i = 0; i < dlugoscWizytowki; i++)
+            {
+                zapis.Write("*");
+            }
+            zapis.WriteLine();
+            // imie+nazwisko
+            zapis.Write("*");
+            s = (dlugoscWizytowki - a) / 2 - 1;
+            for (int i = 0; i < s; i++)
+            {
+                zapis.Write(" ");
+            }
+            zapis.Write(os.imie + " " + os.nazwisko);
+            for (int i = 0; i < s; i++)
+            {
+                zapis.Write(" ");
+            }
+            zapis.Write("*");
+            zapis.WriteLine();
+            // email
+            zapis.Write("*");
+            s = (dlugoscWizytowki - b) / 2 - 1;
+            for (int i = 0; i < s; i++)
+            {
+                zapis.Write(" ");
+            }
+            zapis.Write(os.email);
+            for (int i = 0; i < s; i++)
+            {
+                zapis.Write(" ");
+            }
+            zapis.Write("*");
+            zapis.WriteLine();
+
+            // telefon
+            zapis.Write("*");
+            s = (dlugoscWizytowki - c) / 2 - 1;
+            for (int i = 0; i < s; i++)
+            {
+                zapis.Write(" ");
+            }
+            zapis.Write(napisTelefon);
+            for (int i = 0; i < s; i++)
+            {
+                zapis.Write(" ");
+            }
+            zapis.Write("*");
+            zapis.WriteLine();
+
+            // ostatni wiersz wizytówki
+            for (int i = 0; i < dlugoscWizytowki; i++)
+            {
+                zapis.Write("*");
+            }
+            zapis.WriteLine();
+
+            zapis.Close();
+            plik.Close();
+
+            //Console.WriteLine(os.imie);
+            //Console.WriteLine(os.nazwisko);
+            //Console.WriteLine(os.email);
+            //Console.WriteLine(os.telefon);
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Zadanie3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Zadanie3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * ile
+             * suma
+             * średnia
+             */
+
+            // fragment operacji na plikach
+            string nazwa = @"C:\Users\sagata\Downloads\inne\kursy\C#\zaj5\pliki10\zastepstwo\liczby3.txt";
+            string s; // bieżąca linia
+            double suma = 0;
+
+            FileStream plik = new FileStream(nazwa, FileMode.Open, FileAccess.Read, FileShare.None);
+            System.IO.StreamReader odczyt = new StreamReader(plik);
+
+            while ((s = odczyt.ReadLine())!= null)
+            {
+                Console.WriteLine(s);
+                suma += Convert.ToDouble(s);
+            }
+
+            odczyt.Close();
+            plik.Close();
+            Console.WriteLine("Suma: {0}", suma);
+
+            Console.ReadKey();
+        }
+    }
+}
